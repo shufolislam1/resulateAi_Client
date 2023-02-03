a20bb27b9aaffde3b7b777a94ca6467baeceb238
shufol islam <shufolislam1255@gmail.com>
2023-02-03 20:39:36 +0600

    addstudent form data working

--- src/components/Addstudents.jsx
+++ src/components/Addstudents.jsx
@@ -2,57 +2,90 @@ import React from 'react';
 import Navbar from './Navbar';
 
 const Addstudents = () => {
+    const handleForm = (event) => {
+        event.preventDefault();
+
+        const form = event?.target;
+        const fname = form.fName.value;
+        const mname = form.mName.value;
+        const lname = form.lName.value;
+        const rollNumber = form.rollNumber.value;
+        const adress1 = form.adressLine1.value;
+        const adress2 = form.adressLine2.value;
+        const landmark = form.landmark.value;
+        const city = form.city.value;
+        const pinCode = form.pinCode.value;
+        const selectClasses = form.selectClasses.value;
+        const selectDivisions = form.selectDivisions.value;
+
+        console.log(fname, lname, mname,rollNumber, adress1, adress2, landmark, city, pinCode, selectClasses, selectDivisions);
+        form.reset();
+    }
+
     return (
         <div>
             <Navbar></Navbar>
             <div>
                 <div className="hero min-h-screen">
-                    <form className="hero-content flex-col lg:flex-row-reverse">
-                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                            <div className="card-body">
+                    <form onSubmit={handleForm} className="hero-content flex-col lg:flex-row-reverse">
+                        <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+                            <div className="flex flex-wrap">
                                 <div className=''>
                                     <div className="form-control">
-                                        <input type="text" placeholder="First Name" className="input input-bordered" />
+                                        <input name='fName' type="text" placeholder="First Name" className="input input-bordered" />
                                     </div>
                                     <div className="form-control">
-                                        <input type="text" placeholder="Middle Name" className="input input-bordered" />
+                                        <input name='mName' type="text" placeholder="Middle Name" className="input input-bordered" />
                                     </div>
                                     <div className="form-control">
-                                        <input type="text" placeholder="Last Name" className="input input-bordered" />
+                                        <input name='lName' type="text" placeholder="Last Name" className="input input-bordered" />
                                     </div>
                                 </div>
                                 {/* this part for select many item */}
                                 <div className="form-control w-full max-w-xs">
-                                    <select className="select select-bordered">
+                                    <select name='selectClasses' className="select select-bordered">
                                         <option disabled selected>Select Classes</option>
-                                        <option>Star Wars</option>
-                                        <option>Harry Potter</option>
-                                        <option>Lord of the Rings</option>
-                                        <option>Planet of the Apes</option>
-                                        <option>Star Trek</option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                        <option>8</option>
+                                        <option>9</option>
+                                        <option>10</option>
+                                        <option>11</option>
+                                        <option>12</option>
                                     </select>
                                 </div>
                                 <div className="form-control w-full max-w-xs">
-                                    <select className="select select-bordered">
+                                    <select name='selectDivisions' className="select select-bordered">
                                         <option disabled selected>Select Divisions</option>
-                                        <option>Star Wars</option>
-                                        <option>Harry Potter</option>
-                                        <option>Lord of the Rings</option>
-                                        <option>Planet of the Apes</option>
-                                        <option>Star Trek</option>
+                                        <option>A</option>
+                                        <option>B</option>
+                                        <option>C</option>
+                                        <option>D</option>
+                                        <option>E</option>
                                     </select>
                                 </div>
                                 <div className="form-control">
-                                    <input type="text" placeholder="Adress Line 2" className="input input-bordered" />
+                                    <input name='rollNumber' type="text" placeholder="Roll Number" className="input input-bordered" />
                                 </div>
                                 <div className="form-control">
-                                    <input type="text" placeholder="Landmark" className="input input-bordered" />
+                                    <textarea name='adressLine1' type="text" placeholder="Adress Line 1" className="input input-bordered" />
                                 </div>
                                 <div className="form-control">
-                                    <input type="text" placeholder="City" className="input input-bordered" />
+                                    <textarea name='adressLine2' type="text" placeholder="Adress Line 2" className="input input-bordered" />
                                 </div>
                                 <div className="form-control">
-                                    <input type="text" placeholder="Pin Code" className="input input-bordered" />
+                                    <input name='landmark' type="text" placeholder="Landmark" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <input name='city' type="text" placeholder="City" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <input name='pinCode' type="text" placeholder="Pin Code" className="input input-bordered" />
                                 </div>
                                 <div className="form-control mt-6">
                                     <button className="btn btn-primary">Add Student</button>
