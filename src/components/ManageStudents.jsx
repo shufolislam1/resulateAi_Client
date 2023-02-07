c4a268878c1131de75aa50eb8691270a7838c67f
shufol islam <shufolislam1255@gmail.com>
2023-02-07 21:29:53 +0600

    view button enabled

--- src/components/ManageStudents.jsx
+++ src/components/ManageStudents.jsx
@@ -12,6 +12,7 @@ const ManageStudents = () => {
         fetch('http://localhost:5000/manageStudents')
             .then(res => res.json())
             .then(data => setInfo(data))
+        // .then(data => console.log(data))
     }, [])
     return (
         <div>
@@ -29,41 +30,71 @@ const ManageStudents = () => {
                         </thead>
                         <tbody>
                             <tr>
-                                <td>                                {
-                                    info.map((info, index) => {
-                                        return (
-                                            <div key={index}>
-                                                <td> {info.fname}</td>
-                                            </div>
-                                        )
-                                    })
-                                }</td>
-                                <td>                                {
-                                    info.map((info, index) => {
-                                        return (
-                                            <div key={index}>
-                                                <td>{info.selectClasses}</td>
-                                            </div>
-                                        )
-                                    })
-                                }</td>
-                                <td>                                {
-                                    info.map((info, index) => {
-                                        return (
-                                            <div key={index}>
-                                                <td> {info.rollNumber}</td>
-                                            </div>
-                                        )
-                                    })
-                                }</td>
                                 <td>
                                     {
                                         info.map((info, index) => {
                                             return (
                                                 <div key={index}>
+                                                    <td> {info.fname}</td>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        info.map((info, index) => {
+                                            return (
+                                                <div key={index}>
+                                                    <td>{info.selectClasses}</td>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        info.map((info, index) => {
+                                            return (
+                                                <div key={index}>
+                                                    <td> {info.rollNumber}</td>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        info.map((info, index) => {
+                                            return (
+                                                <div key={index} className=''>
                                                     <div className='flex'>
-                                                        <div>
-                                                            <td><AiOutlineEye></AiOutlineEye></td>
+                                                        <div >
+                                                            <td>
+                                                                {/* The button to open modal */}
+                                                                <label htmlFor="my-modal-5" className="btn btn-xs">
+                                                                    <AiOutlineEye>
+                                                                    </AiOutlineEye>
+                                                                </label>
+
+                                                                {/* Put this part before </body> tag */}
+                                                                <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+                                                                <div className="modal">
+                                                                    <div className="modal-box w-11/12 max-w-5xl">
+                                                                        <h3 className="font-bold text-lg">Name : {info?.fname} {info?.mname} {info?.lname}</h3>
+                                                                        <h3 className="font-bold text-lg">City: {info?.city}</h3>
+                                                                        <h3 className="font-bold text-lg">Adress 1: {info?.adress1}</h3>
+                                                                        <h3 className="font-bold text-lg">Adress 2: {info?.adress2}</h3>
+                                                                        <h3 className="font-bold text-lg">Pin-Code: {info?.pinCode}</h3>
+                                                                        <h3 className="font-bold text-lg">Roll: {info?.rollNumber}</h3>
+                                                                        <h3 className="font-bold text-lg">Class: {info?.selectClasses}</h3>
+                                                                        <h3 className="font-bold text-lg">Division: {info?.selectDivisions}</h3>
+                                                                        <div className="modal-action">
+                                                                            <label htmlFor="my-modal-5" className="btn">ok</label>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </td>
                                                         </div>
                                                         <div>
                                                             <td><AiFillEdit></AiFillEdit></td>
@@ -76,41 +107,8 @@ const ManageStudents = () => {
                                             )
                                         })
                                     }
-
                                 </td>
                             </tr>
-                            {/* <tr>
-                                <td>Hart Hagerty</td>
-                                <td>Desktop Support Technician</td>
-                                <td>Purple</td>
-                                <div className='flex'>
-                                    <div>
-                                        <td><AiOutlineEye></AiOutlineEye></td>
-                                    </div>
-                                    <div>
-                                        <td><AiFillEdit></AiFillEdit></td>
-                                    </div>
-                                    <div>
-                                        <td> <AiOutlineRest></AiOutlineRest></td>
-                                    </div>
-                                </div>
-                            </tr>
-                            <tr>
-                                <td>Brice Swyre</td>
-                                <td>Tax Accountant</td>
-                                <td>Red</td>
-                                <div className='flex'>
-                                    <div>
-                                        <td><AiOutlineEye></AiOutlineEye></td>
-                                    </div>
-                                    <div>
-                                        <td><AiFillEdit></AiFillEdit></td>
-                                    </div>
-                                    <div>
-                                        <td> <AiOutlineRest></AiOutlineRest></td>
-                                    </div>
-                                </div>
-                            </tr> */}
                         </tbody>
                     </table>
                 </div>
